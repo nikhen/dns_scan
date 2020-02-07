6001c4e710f06e8d718070d98d416776904da317
nikhen <niko@heners-net.de>
2020-02-07 10:52:09 -0500

    Added function to check command line arguments

--- dns_scan.sh
+++ dns_scan.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
+function print_usage_disclaimer() {
+        echo "Please run this script with two command line arguments."
+        echo "Usage:"
+        echo "    bash dns_scan.sh IP_ADDRESS PORT_NUMBER"
+	echo ""
+}
+
 function check_arguments() {
-    echo "Checking command line arguments; show useage for invalid arguments"
-
-    echo "Check ip syntax"
-
-    echo "Check port"
+    if [ $# -eq 2 ]; then
+        if [[ $2 =~ ^-?[0-9]+$ ]]; then
+            echo "Scanning port" $2
+        else
+            print_usage_disclaimer
+            echo "PORT_NUMBER has to be integer."
+            exit 1
+        fi
+    else
+        echo $# "command line arguments."
+        print_usage_disclaimer
+        exit 1
+    fi
 }
 
 function run_port_scan() {
     echo "Running port scans on dns port"
+
+    nmap -sSU -p $port --script dns-nsid $ip
 }
 
 function display_result() {
