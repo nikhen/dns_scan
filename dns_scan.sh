47c6eb01bc7beaf471d420a3cb5d688e9e842e80
nikhen <niko@heners-net.de>
2020-02-08 09:38:07 +0100

    - Change order in which port scans are executed - add script arguments - add hostname as command line parameter

--- dns_scan.sh
+++ dns_scan.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function print_usage_disclaimer() {
-        echo "Please run this script with two command line arguments."
+        echo "Please run this script with three command line arguments."
         echo "Usage:"
-        echo "    bash dns_scan.sh IP_ADDRESS PORT_NUMBER"
+        echo "    bash dns_scan.sh IP_ADDRESS PORT_NUMBER HOSTNAME"
 	echo ""
 }
 
 function check_arguments() {
-    if [ $# -eq 2 ]; then
+    if [ $# -eq 3 ]; then
         if [[ $2 =~ ^-?[0-9]+$ ]]; then
             echo "Scanning port" $2
         else
@@ -24,16 +24,18 @@ function check_arguments() {
 }
 
 function run_port_scan() {
+    local timelimit=5m
+
     echo "Running port scans on dns port" $port
+    echo "Timelimit is set to" $timelimit
 
     nmap -sSU -p $port --script=dns-nsid.nse $ip
-    nmap -sSU -p $port --script=dns-srv-enum.nse $ip
-    nmap -sSU -p $port --script=dns-fuzz.nse $ip
-    nmap -sSU -p $port --script=dns-update.nse $ip
-    nmap -sSU -p $port --script=dns-ip6-arpa-scan.nse $ip
-    nmap -sSU -p $port --script=dns-zeustracker.nse $ip
-    nmap -sSU -p $port --script=dns-nsec3-enum.nse $ip
+    nmap -sSU -p $port --script=dns-update.nse --script-args=dns-update.hostname=dnswizard.com,dns-update.ip=192.0.2.1 $ip
+    nmap -sn -PN --script=dns-zeustracker $ip
+    nmap -sSU -p $port --script=dns-nsec3-enum.nse --script-args dns-nsec3-enum.domains=$domain $ip
     nmap -sSU -p $port --script=dns-zone-transfer.nse $ip
+    nmap --script=dns-srv-enum.nse --script-args "dns-srv-enum.domain="$domain $ip -v
+    nmap -sSU -p $port --script=dns-fuzz.nse $ip --script-args timelimit=$timelimit
 }
 
 function display_result() {
@@ -43,13 +45,15 @@ function display_result() {
 function main() {
     local ip=$1
     local port=$2
-    echo "Starting Test Scan for IP" $ip $port
+    local domain=$3
 
-    check_arguments $ip $port
+    echo "Starting Test Scan for IP" $ip $port ". Domain name is" $domain
+
+    check_arguments $ip $port $domain
  
-    run_port_scan $ip $port
+    run_port_scan $ip $port $domain
 
     display_result
 }
 
-main $1 $2
+main $1 $2 $3
