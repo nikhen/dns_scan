25ebb1b1236c06a87bac6dfb09cc13f67c3f6720
nikhen <niko@heners-net.de>
2020-02-08 13:12:55 +0100

    fix segmentation fault 2

--- dns_scan.sh
+++ dns_scan.sh
@@ -30,6 +30,7 @@ function check_arguments() {
 function run_port_scan() {
     local timelimit=5m
     local enum_script_arguments="dns-srv-enum.domain="$domain
+    local dns_fuzz_arguments="timelimit="$timelimit
 
     print_variable "Timelimit is set to" $timelimit
 
@@ -39,7 +40,7 @@ function run_port_scan() {
     nmap -sSU -p $port --script=dns-nsec3-enum.nse --script-args dns-nsec3-enum.domains=$domain $ip
     nmap -sSU -p $port --script=dns-zone-transfer.nse $ip
     nmap -sn --script=dns-srv-enum.nse --script-args $enum_script_arguments $ip
-    nmap -sSU -p $port --script=dns-fuzz.nse $ip --script-args timelimit=$timelimit
+    nmap -sSU -p $port --script=dns-fuzz.nse $ip --script-args $dns_fuzz_arguments
 }
 
 function display_result() {
